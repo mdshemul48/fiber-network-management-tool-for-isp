31c7764ee6eabe3762d37cb09e9a7df672b5a31c
MD. Shimul <mdshemul480@gmail.com>
2022-02-15 13:34:10 +0600

    adding main local connection

--- newProject/js/addNewMainConnection/addMainLocalConnection.js
+++ newProject/js/addNewMainConnection/addMainLocalConnection.js
@@ -1,3 +1,6 @@
+import Graph from '../storage/Graph.js';
+import uuidv4 from '../utility/uuid.js';
+
 export default (polylineKey, coordinates) => {
   const connectionName = document.getElementById(
     'addLocalConnectionName'
@@ -7,19 +10,33 @@ export default (polylineKey, coordinates) => {
   ).value;
 
   const portNo = document.getElementById('addLocalConnectionPortNo').value;
+
   const connectionType = document.querySelector(
     'input[name="addLocalConnectionType"]:checked'
   ).value;
+
+  const coreColor = document.getElementById(
+    'addLocalConnectionCoreOption'
+  ).value;
+
+  console.log(coreColor);
 
   const newConnection = {
     connectionName,
     oldSwitchNumber,
     portNo,
-    connectionType,
+    connectionType: 'mainLocal',
+    switchType: connectionType,
     coordinates,
+    coreColor,
     totalConnection: connectionType === 'epon' ? 64 : 128,
     childrenConnection: {},
     totalConnectionUsed: 0,
   };
-  console.log(newConnection);
+
+  const graph = new Graph(JSON.parse(localStorage.getItem('siteData')) || null);
+  const uuid = uuidv4();
+  graph.addVertex(uuid, newConnection);
+  graph.addEdge(polylineKey, uuid, coreColor);
+  console.log(graph);
 };
