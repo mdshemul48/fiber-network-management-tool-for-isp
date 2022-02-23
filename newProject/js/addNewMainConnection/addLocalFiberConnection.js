d2e2fb3a39913b3414d7f3f835d5351a950f91fc
MD. Shimul <mdshemul480@gmail.com>
2022-02-23 23:02:33 +0600

    adding fiber adding option

--- newProject/js/addNewMainConnection/addLocalFiberConnection.js
+++ newProject/js/addNewMainConnection/addLocalFiberConnection.js
@@ -1,3 +1,18 @@
+import coreColor from '../utility/coreColor.js';
+
 export default (polylineKey, coordinates) => {
-  console.log(polylineKey, coordinates);
+  const connectionName = document.getElementById(
+    'addLocalFiberConnectionName'
+  ).value;
+
+  const connectionTotalCore = Number(
+    document.getElementById('addLocalFiberConnectionCoreOptions').value
+  );
+
+  const selectedCoreColor = coreColor.slice(0, connectionTotalCore);
+
+  const connectionCoreColor = {};
+  selectedCoreColor.forEach((item) => {
+    connectionCoreColor[item.colorName] = null;
+  });
 };
