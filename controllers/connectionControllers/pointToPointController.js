c19bc69f0708e447d8cd84a81e8430598c374cd1
MD. Shimul <mdshemul480@gmail.com>
2022-03-06 21:56:47 +0600

    adding validation arr

--- controllers/connectionControllers/pointToPointController.js
+++ controllers/connectionControllers/pointToPointController.js
@@ -1,15 +1,38 @@
+const { body, validationResult } = require('express-validator');
 const pointToPointConnectionModel = require('../../model/pointToPointConnectionModel.js');
 
 // creating all the main point to point connection controller functions
 
+exports.createPointToPointConnectionValidation = [
+  body('name').isEmpty().withMessage('name is required'),
+
+  body('totalCore')
+    .isEmpty()
+    .withMessage('totalCore is required')
+    .isInt({ min: 2 })
+    .withMessage('totalCore must be an integer or greater than 2'),
+
+  body('coordinates')
+    .isEmpty()
+    .withMessage('coordinates is required')
+    .isArray()
+    .withMessage('coordinates must be an array'),
+];
+
 exports.createPointToPointConnection = async (req, res) => {
   try {
+    // validating the request body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { name, totalCore, coordinates } = req.body;
     const coordinatesLatLngArr = coordinates.map((item) => {
       return [item.lat, item.lng];
     });
     const createdConnection = await pointToPointConnectionModel.create({
-      name: connectionName,
+      name,
       totalCore,
       location: { coordinates: coordinatesLatLngArr },
       childrenConnection: [],
