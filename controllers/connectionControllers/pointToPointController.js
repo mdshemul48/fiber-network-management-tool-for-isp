cec5c08ba0fc28bfc72b3f4442c5c76fb0f593d1
MD. Shimul <mdshemul480@gmail.com>
2022-03-06 19:54:47 +0600

    adding controllers

--- controllers/connectionControllers/pointToPointController.js
+++ controllers/connectionControllers/pointToPointController.js
@@ -1,3 +1,4 @@
-module.exports.createPointToPointConnection = (req, res) => {
+// creating all the main point to point connection controller functions
+exports.createPointToPointConnection = (req, res) => {
   res.send('from point to point');
 };
--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+// importing all the controllers
 const {
   createPointToPointConnection,
 } = require('../controllers/connectionControllers/pointToPointController.js');
 
+// creating routes
 router.get('/create-ptp-connection', createPointToPointConnection);
 
 module.exports = router;
