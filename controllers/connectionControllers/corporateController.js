1cc073bdd1a2ad400dc0c09f60c5ad1a32a36076
MD. Shimul <mdshemul480@gmail.com>
2022-03-07 15:54:12 +0600

    adding try catch

--- controllers/connectionControllers/corporateController.js
+++ controllers/connectionControllers/corporateController.js
@@ -1,36 +1,42 @@
 const pointToPointConnectionModel = require('../../model/pointToPointConnectionModel.js');
-
 const corporateConnectionModel = require('../../model/corporateConnectionModel.js');
 
 module.exports.createCorporateConnection = async (req, res) => {
-  const { parent, name, portNo, coreColor, coordinates } = req.body;
+  try {
+    const { parent, name, portNo, coreColor, coordinates } = req.body;
 
-  const parentConnection = await pointToPointConnectionModel.findById(parent);
+    const parentConnection = await pointToPointConnectionModel.findById(parent);
 
-  const coordinatesLatLngArr = coordinates.map((item) => {
-    return [item.lat, item.lng];
-  });
+    const coordinatesLatLngArr = coordinates.map((item) => {
+      return [item.lat, item.lng];
+    });
 
-  const createdCorporateConnection = await corporateConnectionModel.create({
-    parentType: parentConnection.type,
-    parent: parentConnection._id.toString(),
-    name,
-    portNo,
-    color: coreColor,
-    location: { coordinates: coordinatesLatLngArr },
-  });
+    const createdCorporateConnection = await corporateConnectionModel.create({
+      parentType: parentConnection.type,
+      parent: parentConnection._id.toString(),
+      name,
+      portNo,
+      color: coreColor,
+      location: { coordinates: coordinatesLatLngArr },
+    });
 
-  parentConnection.childrens.push({
-    color: coreColor,
-    connectionType: 'corporate',
-    child: createdCorporateConnection._id.toString(),
-  });
+    parentConnection.childrens.push({
+      color: coreColor,
+      connectionType: 'corporate',
+      child: createdCorporateConnection._id.toString(),
+    });
 
-  parentConnection.totalConnected++;
-  parentConnection.save();
+    parentConnection.totalConnected++;
+    parentConnection.save();
 
-  return res.status(201).json({
-    status: 'success',
-    data: createdCorporateConnection,
-  });
+    return res.status(201).json({
+      status: 'success',
+      data: createdCorporateConnection,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 };
