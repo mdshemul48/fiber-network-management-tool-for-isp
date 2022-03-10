70dbbf5c8349d257c85f92665ccab84906cc8ee0
MD. Shimul <mdshemul480@gmail.com>
2022-03-10 16:03:26 +0600

    removing unused code

--- static/js/addNewMainConnection/addMainLocalConnection.js
+++ static/js/addNewMainConnection/addMainLocalConnection.js
@@ -1,6 +1,3 @@
-import Graph from '../storage/Graph.js';
-import uuidv4 from '../utility/uuid.js';
-
 export default async (polylineKey, coordinates) => {
   const connectionName = document.getElementById(
     'addLocalConnectionName'
