38a22e711c98bcef8187abdd794dcd8d6670643c
MD. Shimul <mdshemul480@gmail.com>
2022-03-10 20:26:34 +0600

    adding localFIberData on getAllConnection controller

--- controllers/getAllConnection.js
+++ controllers/getAllConnection.js
@@ -1,12 +1,20 @@
 const pointToPointConnectionModel = require('../model/pointToPointConnectionModel.js');
 const corporateConnectionModel = require('../model/corporateConnectionModel.js');
 const resellerConnectionModel = require('../model/resellerConnectionModel.js');
+const createLocalFiberModel = require('../model/localFiberConnectionModel.js');
+
 exports.getAllConnection = async (req, res) => {
   const pointToPointData = await pointToPointConnectionModel.find();
   const corporateData = await corporateConnectionModel.find();
   const resellerData = await resellerConnectionModel.find();
+  const localFiberData = await createLocalFiberModel.find();
   return res.status(200).json({
     status: 'success',
-    data: [...pointToPointData, ...corporateData, ...resellerData],
+    data: [
+      ...pointToPointData,
+      ...corporateData,
+      ...resellerData,
+      ...localFiberData,
+    ],
   });
 };
