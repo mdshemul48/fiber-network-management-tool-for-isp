ddca565366da715e901e3283d7ee4809a50eb35f
MD. Shimul <mdshemul480@gmail.com>
2022-03-14 14:41:56 +0600

    adding splitter connection

--- controllers/connectionControllers/splitterController.js
+++ controllers/connectionControllers/splitterController.js
@@ -75,5 +75,56 @@ exports.createSplitterConnection = async (req, res) => {
         message: 'Invalid localFiber id',
       });
     }
+
+    const reseller = await resellerConnectionModel.findById(
+      localFiber.parent.toString()
+    );
+    if (!reseller) {
+      return res.json({
+        status: 'error',
+        message: 'Invalid reseller id',
+      });
+    }
+
+    const alreadyExistSplitter = reseller.childrens.find((item) => {
+      return (
+        item.connectionType === 'splitter' &&
+        (item.color === color || item.portNo === parseInt(portNo))
+      );
+    });
+
+    if (alreadyExistSplitter) {
+      return res.status(400).json({
+        status: 'error',
+        message:
+          'localFiber already has a splitter with the same color or port number',
+      });
+    }
+
+    const splitterConnection = await splitterConnectionModel.create({
+      parentType: 'localFiber',
+      parent: localFiber._id,
+      reseller: reseller._id,
+      name,
+      splitterLimit,
+      portNo,
+      location: {
+        coordinates: coordinatesLatLngArr,
+      },
+    });
+
+    reseller.childrens.push({
+      portNo,
+      connectionType: 'splitter',
+      child: splitterConnection._id,
+      connectionUsed: 0,
+    });
+
+    await reseller.save();
+
+    return res.status(200).json({
+      status: 'success',
+      data: splitterConnection,
+    });
   }
 };
