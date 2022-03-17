7026568746986cf2e153c96c0f26d949d98a9a9f
MD. Shimul <mdshemul480@gmail.com>
2022-03-17 14:48:44 +0600

    adding property to the home model

--- model/homeConnectionModel.js
+++ model/homeConnectionModel.js
@@ -5,8 +5,21 @@ const {
 } = require('mongoose');
 
 const homeConnectionSchema = Schema({
+  parentType: {
+    type: String,
+    enum: ['home'],
+    required: true,
+  },
+
+  parent: {
+    type: ObjectId,
+    refPath: 'parentType',
+  },
+
   name: {
     type: String,
     required: true,
   },
 });
+
+const HomeConnection = model('HomeConnection', homeConnectionSchema);
