06d197ce80baedb5fbd2291c92f6b7e3f4617fda
MD. Shimul <mdshemul480@gmail.com>
2022-04-06 13:23:59 +0600

    adding timestamp

--- model/homeConnectionModel.js
+++ model/homeConnectionModel.js
@@ -4,48 +4,51 @@ const {
   Types: { ObjectId },
 } = require('mongoose');
 
-const homeConnectionSchema = Schema({
-  parentType: {
-    type: String,
-    enum: ['splitter'],
-    required: true,
-  },
-
-  parent: {
-    type: ObjectId,
-    refPath: 'parentType',
-  },
-
-  name: {
-    type: String,
-    required: true,
-  },
-
-  type: {
-    type: String,
-    default: 'home',
-  },
-
-  onuNo: {
-    type: String,
-    required: true,
-  },
-
-  color: {
-    type: String,
-    required: true,
-  },
-
-  locations: {
-    type: {
+const homeConnectionSchema = Schema(
+  {
+    parentType: {
       type: String,
-      default: 'LineString',
-    },
-    coordinates: {
-      type: [[Number]],
+      enum: ['splitter'],
       required: true,
     },
+
+    parent: {
+      type: ObjectId,
+      refPath: 'parentType',
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      default: 'home',
+    },
+
+    onuNo: {
+      type: String,
+      required: true,
+    },
+
+    color: {
+      type: String,
+      required: true,
+    },
+
+    locations: {
+      type: {
+        type: String,
+        default: 'LineString',
+      },
+      coordinates: {
+        type: [[Number]],
+        required: true,
+      },
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = model('home', homeConnectionSchema);
--- model/resellerConnectionModel.js
+++ model/resellerConnectionModel.js
@@ -4,91 +4,94 @@ const {
   Types: { ObjectId },
 } = require('mongoose');
 
-const resellerConnectionSchema = new Schema({
-  parentType: {
-    type: String,
-    default: 'pointToPoint',
-    enum: ['pointToPoint'],
-  },
-  parent: { type: ObjectId, refPath: 'parentType' },
-
-  name: {
-    type: String,
-    required: true,
-  },
-
-  type: {
-    type: String,
-    default: 'reseller',
-  },
-
-  portNo: {
-    type: Number,
-    required: true,
-  },
-
-  oltType: {
-    type: String,
-    required: true,
-    enum: ['epon', 'gpon'],
-  },
-
-  oltSerialNumber: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-
-  connectionLimit: {
-    type: Number,
-    required: true,
-    enum: [64, 128],
-  },
-
-  connectionUsed: {
-    type: Number,
-    default: 0,
-    required: () => {
-      return this.connectionUsed < this.connectionLimit;
-    },
-  },
-
-  location: {
-    type: {
+const resellerConnectionSchema = new Schema(
+  {
+    parentType: {
       type: String,
-      default: 'LineString',
+      default: 'pointToPoint',
+      enum: ['pointToPoint'],
     },
-    coordinates: {
-      type: [[Number]],
+    parent: { type: ObjectId, refPath: 'parentType' },
+
+    name: {
+      type: String,
       required: true,
     },
-  },
 
-  childrens: [
-    {
-      connectionType: {
-        type: String,
-        required: true,
-        enum: ['splitter', 'localFiber'],
-      },
-      portNo: {
-        type: Number,
-        required: false,
-      },
-      child: {
-        type: ObjectId,
-        required: true,
-        refPath: 'connectionType',
-      },
-      connectionUsed: {
-        type: Number,
-        required: false,
+    type: {
+      type: String,
+      default: 'reseller',
+    },
+
+    portNo: {
+      type: Number,
+      required: true,
+    },
+
+    oltType: {
+      type: String,
+      required: true,
+      enum: ['epon', 'gpon'],
+    },
+
+    oltSerialNumber: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+
+    connectionLimit: {
+      type: Number,
+      required: true,
+      enum: [64, 128],
+    },
+
+    connectionUsed: {
+      type: Number,
+      default: 0,
+      required: () => {
+        return this.connectionUsed < this.connectionLimit;
       },
     },
-  ],
-});
+
+    location: {
+      type: {
+        type: String,
+        default: 'LineString',
+      },
+      coordinates: {
+        type: [[Number]],
+        required: true,
+      },
+    },
+
+    childrens: [
+      {
+        connectionType: {
+          type: String,
+          required: true,
+          enum: ['splitter', 'localFiber'],
+        },
+        portNo: {
+          type: Number,
+          required: false,
+        },
+        child: {
+          type: ObjectId,
+          required: true,
+          refPath: 'connectionType',
+        },
+        connectionUsed: {
+          type: Number,
+          required: false,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = model('reseller', resellerConnectionSchema);
